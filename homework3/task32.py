183ba65635604de9c37684d17b37144f18d0ac97
bnny28 <bnny28@gmail.com>
2022-07-13 13:06:02 +0300

    Убрал Union

--- homework3/task32.py
+++ homework3/task32.py
@@ -5,7 +5,7 @@ def delay_decorator_runner(call_count, start_sleep_time, factor, boarder_sleep_t
     """
     Вычисляет функцию заданное количество раз с задержкой, определяемой по правилам:
 
-    t = start_sleep_time * factor if t < border_sleep_time
+    t = start_sleep_time * factor ^ n if t < border_sleep_time
     t = border_sleep_time if t >= border_sleep_time.
 
     :param call_count: количество запусков функции
